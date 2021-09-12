0cc3b84d65f6687fe0b1a70b8db6ef585217d2ed
hananawad12 <hananawad@gmail.com>
2021-09-12 18:06:27 +0200

    Update task4.py

--- src/task4.py
+++ src/task4.py
@@ -1,6 +1,6 @@
 def decorator_4(fun):
     def wrapper(*args):
-        fn(*args)   # this is an error (I write fn istead of fun): I put it in log file with atimestamp
+        fn(*args)   # this is an error (I write fn istead of fun): I put it in log file with a timestamp
         
 
     return wrapper
